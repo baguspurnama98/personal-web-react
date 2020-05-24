24058cf1c585c71f002c6761b325c7a8fd77207e
Cornelius Bagus Purnama Putra <baguspurnama98@gmail.com>
2020-05-24 19:24:27 +0700

    Updates

---
+++ precache-manifest.4d71fe548446b3387facfc5398a960e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d045b9ddaa28591569ddf57b11af8c",
+    "url": "/personalWeb/index.html"
+  },
+  {
+    "revision": "608577df27636d3d7be9",
+    "url": "/personalWeb/static/js/2.83c9dc4b.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/personalWeb/static/js/2.83c9dc4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7905a1ebee0bcebe67e6",
+    "url": "/personalWeb/static/js/main.6017be70.chunk.js"
+  },
+  {
+    "revision": "a400ee8f787d8144e060",
+    "url": "/personalWeb/static/js/runtime-main.bfb01543.js"
+  }
+]);
